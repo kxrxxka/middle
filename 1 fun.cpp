5984e927e2234901c3da96b2d0fd726753666460
kxrxxka <145845389+kxrxxka@users.noreply.github.com>
2024-04-01 13:33:36 +0300

    Update 1 fun.cpp

--- 1 fun.cpp
+++ 1 fun.cpp
@@ -9,14 +9,14 @@ void itc_num_print(int number) {
 int itc_len_num(long long number) {
     
     int num = 0;
-    while (number != 0) {
-        number = number / 10;
-        num++;
-    }
-    
+
     if (number == 0) {
         num = 1;
     }
+    while (number != 0) {
+        number = number / 10;
+        num++;
+    }  
     return num;
 
 }
