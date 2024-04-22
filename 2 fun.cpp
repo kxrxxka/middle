e47a8800941b28638ef49cf0ac50913ed93fcbee
kxrxxka <145845389+kxrxxka@users.noreply.github.com>
2024-04-22 08:51:24 +0300

    Update 2 fun.cpp

--- 2 fun.cpp
+++ 2 fun.cpp
@@ -3,16 +3,20 @@ using namespace std;
 
 int itc_min_num(long long number) {
 
+     if (number <= 0) {
+        return -1;
+    }
+
     int num;
     int minn = 9;
+
     while (number > 0) {
         num = number % 10;
+    }
         if (num < minn) {
             minn = num;
         }
         number = number / 10;
     }
     return minn;
-
-
 }
