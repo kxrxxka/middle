53b8dd0f930b93660b9b212010691817f45fb61c
kxrxxka <145845389+kxrxxka@users.noreply.github.com>
2024-05-13 14:21:52 +0300

    Update 1 fun.cpp

--- 1 fun.cpp
+++ 1 fun.cpp
@@ -40,21 +40,22 @@ int itc_sum_num(long long number) {
 
 long long itc_multi_num(long long number) {
 
-       int mult = 1;
-      
-      if (number <= 0) {
-          return -1;
-      }
-    
-     while (number > 0) {
-        mult = mult * (number % 10);
-        number = number / 10;
+
+    if (number == 0){
+        return -1;
     }
     
-    if (number < 0) {
+    if (number < 0){
         number = number * (-1);
     }
     
+    int num = 1;
+    int mult = 1;
+    while (number > 0){
+        num = number % 10;
+        mult = mult * num;
+        number /= 10;
+    }
     return mult;
     
 }
