28aeea9a6ee1913143dd9ef007d83da709a70cd1
kxrxxka <145845389+kxrxxka@users.noreply.github.com>
2024-05-20 05:01:25 +0300

    Update 2 fun.cpp

--- 2 fun.cpp
+++ 2 fun.cpp
@@ -3,49 +3,110 @@ using namespace std;
 
 int itc_min_num(long long number) {
 
+ 
+ int minn = 9;
+ long long num;
+    
     if (number == 0){
-        return -1;
+        minn = 0;
     }
     
     if (number < 0){
-        number = number * (-1);
-    }
+    number = number * (-1);
+}
     
-    int num;
-    int minn = 9;
-
-    while (number > 0) {
+    while (number != 0) {
+        
         num = number % 10;
-    
         if (num < minn) {
             minn = num;
         }
+        
         number = number / 10;
     }
     return minn;
+
 }
 
-int itc_rev_num(long long number) {
+
+int itc_rev_num(long long number){
     
-    if (number == 0){
-        return -1;
-    }
-    
-    if (number < 0){
-        number = number * (-1);
-    }
-   
-   long long num;
-   long long numn;
-   numn = 0;
-     
-   while(number > 0) {
-       num = number % 10;
-       numn = numn * 10;
-       numn = numn + num;
-       number = number / 10;
-       
-   }
-   
-   return numn;
+int n = 0;
+long long num = 0;
+
+if (number <= 0) {
+    return -1;
 }
+
+while (number > 0){
+    
+    num = number % 10;
+    n = n * 10;
+    n = n + num;
+    number = number / 10;
+}
+
+return itc_len_num(n);
+
+}
+
+itc_null_count(long long number){
+    
+int num = 0;
+
+if (number < 0) {
+    number = number * (-1);
+}
+
+if (number == 0){
+    num = 1;
+}
+
+while (number > 0){
+    
+    if (number % 10 == 0){
+        num ++;
+    }
+    number = number / 10;
+}
+
+return num;
+
+}
+
+itc_mirror_num(long long number){
+    
+int n = 0;
+long long num = 0;
+long long number1 = number;
+
+while (number != 0){
+    n = number % 10;
+    num = num * 10;
+    num = num + n;
+    number = number / 10;
+}
+
+if (num == number1){
+    return 1;
+}
+
+else {
+    return 0;
+}
+
+}
+
+int itc_mirror_count(long long number) {
+long long n = 1;
+int num = 0;
+while (n <= number) {
+    if (itc_mirror_num(n)) {
+        num++;
+    }
+    n++;
+}
+
+return num;
+}
+
