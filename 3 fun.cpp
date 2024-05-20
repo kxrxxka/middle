b05a1450b19fe062bd86e352f8954be73432fb88
kxrxxka <145845389+kxrxxka@users.noreply.github.com>
2024-05-20 15:16:39 +0300

    Update 3 fun.cpp

--- 3 fun.cpp
+++ 3 fun.cpp
@@ -12,12 +12,8 @@ if (number < 0) {
     number = number * (-1);
 }
 
-if (number / 10 == 0) {
-    return - 1;
-}
 
-else {
-    while (number != 0) {
+    while (number > 0) {
 
         n = number % 10;
 
@@ -26,15 +22,15 @@ else {
             maxx1 = n;
         }
 
-        if ((n < maxx1) && (n > maxx1)) {
+        if ((n < maxx1) && (n > maxx2)) {
             maxx2 = n;
         }
 
         number = number / 10;
-    }
+}
 
 return maxx2;
-}
+
 }
 
 long long itc_bin_num(long long number) {
